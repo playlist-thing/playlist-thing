160989f13584150a4c167f591e94cc482f04663c
joachimschmidt557 <joachim.schmidt557@outlook.com>
2025-04-11 08:39:56 +0200

    add first version of idb schema

---
+++ src/lib/db.ts
@@ -0,0 +1,78 @@
+import { openDB } from 'idb';
+import type { DBSchema } from 'idb';
+
+import type { Playlist, Show, Station, DJ } from '$lib/playlist.ts';
+
+interface DBv1 extends DBSchema {
+  playlists: {
+    key: string;
+    value: Playlist;
+    indexes: {
+      slug: string;
+      broadcastStart: number;
+      createdAt: number;
+      lastModifiedAt: number;
+      showId: string;
+      djId: string;
+    };
+  };
+  shows: {
+    key: string;
+    value: Show;
+    indexes: {
+      slug: string;
+      stationId: string;
+      djId: string;
+    };
+  };
+  stations: {
+    key: string;
+    value: Station;
+    indexes: {
+      slug: string;
+      djId: string;
+    };
+  };
+  djs: {
+    key: string;
+    value: DJ;
+    indexes: {
+      slug: string;
+    };
+  };
+}
+
+async function openDatabase() {
+  const db = await openDB<DBv1>('playlist-thing', 1, {
+    async upgrade(db, oldVersion) {
+      const playlists = db.createObjectStore('playlists', {
+        keyPath: 'id'
+      });
+      playlists.createIndex('slug', 'slug', { unique: false });
+      playlists.createIndex('broadcastStart', 'broadcastStart', { unique: false });
+      playlists.createIndex('createdAt', 'createdAt', { unique: false });
+      playlists.createIndex('lastModifiedAt', 'lastModifiedAt', { unique: false });
+      playlists.createIndex('showId', 'showId', { unique: false });
+      playlists.createIndex('djId', 'djIds', { unique: false, multiEntry: true });
+
+      const shows = db.createObjectStore('shows', {
+        keyPath: 'id'
+      });
+      shows.createIndex('slug', 'slug', { unique: true });
+      shows.createIndex('stationId', 'stationId', { unique: false });
+      shows.createIndex('djId', 'djIds', { unique: false, multiEntry: true });
+
+      const stations = db.createObjectStore('stations', {
+        keyPath: 'id'
+      });
+      stations.createIndex('slug', 'slug', { unique: true });
+      stations.createIndex('djId', 'djIds', { unique: false, multiEntry: true });
+
+      const djs = db.createObjectStore('djs', {
+        keyPath: 'id'
+      });
+      djs.createIndex('slug', 'slug', { unique: true });
+    }
+  });
+  return db;
+}
