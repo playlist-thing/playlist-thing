baa29e88f733a93886973fe6ad3762c9f4d581b3
joachimschmidt557 <joachim.schmidt557@outlook.com>
2025-10-12 10:14:07 +0200

    search.ts: add google

--- src/lib/editor/search.ts
+++ src/lib/editor/search.ts
@@ -10,7 +10,8 @@ export const defaultSearchProviders: SearchProvider[] = [
   { id: 0, name: 'YouTube Music', url: 'https://music.youtube.com/search?q=' },
   { id: 1, name: 'Spotify', url: 'https://open.spotify.com/search/' },
   { id: 2, name: 'Apple Music', url: 'https://music.apple.com/search?term=' },
-  { id: 3, name: 'Deezer', url: 'https://www.deezer.com/search/' }
+  { id: 3, name: 'Deezer', url: 'https://www.deezer.com/search/' },
+  { id: 4, name: 'Google', url: 'https://www.google.com/search?q=' }
 ];
 
 export function searchUrl(song: SongMetadata, searchUrl: string) {
