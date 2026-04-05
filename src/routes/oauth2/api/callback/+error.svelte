57befb0d405996464862c8fa29aea041926d72ba
joachimschmidt557 <joachim.schmidt557@outlook.com>
2026-04-05 07:43:57 +0200

    add /oauth2/api/callback route

---
+++ src/routes/oauth2/api/callback/+error.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { page } from '$app/state';
+</script>
+
+<svelte:head>
+  <title>API authorization error</title>
+</svelte:head>
+
+<div class="content-container">
+  <div class="content">
+    <h1>Sorry, an error occurred</h1>
+
+    <p>
+      {page.error!.message}
+    </p>
+
+    <p>Try signing again. If an error still occurs, clear your browser cache and reload.</p>
+
+    <p>
+      If an error still occurs,
+      <a
+        href="https://github.com/playlist-thing/playlist-thing/issues/new"
+        rel="external"
+        target="_blank"
+      >
+        file a bug report
+      </a>
+      with a detailed description of your problem and steps to reproduce it.
+    </p>
+
+    <p>
+      <a href="/editor">Back to editor</a>
+    </p>
+  </div>
+</div>
+
+<style>
+  .content-container {
+    display: flex;
+    justify-content: center;
+  }
+
+  .content {
+    max-width: 450px;
+  }
+</style>
---
+++ src/routes/oauth2/api/callback/+page.ts
@@ -0,0 +1,21 @@
+import { redirect, error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+import { getToken } from '$lib/editor/external/auth/api.ts';
+
+// We require access to sessionStorage for the code verifier and state
+export const ssr = false;
+
+export const load: PageLoad = async ({ url }) => {
+  try {
+    await getToken(url);
+  } catch (e) {
+    if (e instanceof Error) {
+      error(400, e.message);
+    } else {
+      error(500, 'Unknown error');
+    }
+  }
+
+  redirect(302, '/editor');
+};
