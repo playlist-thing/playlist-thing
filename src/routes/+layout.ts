efa7ad82d1e0062bf927872f75dcf0004d13321c
joachimschmidt557 <joachim.schmidt557@outlook.com>
2026-08-07 23:16:03 +0200

    add layout.css with common layout styles

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,6 +2,7 @@ import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import '@fontsource-variable/work-sans';
 
 import '$lib/style/base.css';
+import '$lib/style/layout.css';
 import '$lib/style/a11y.css';
 import '$lib/style/colors.css';
 import '$lib/style/dropdown.css';
