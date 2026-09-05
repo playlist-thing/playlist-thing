7cc4c007653b1ac64e975c4c9814713ca298f930
joachimschmidt557 <joachim.schmidt557@outlook.com>
2026-09-05 22:40:37 +0200

    Item: extract Metadata

---
+++ src/lib/editor/panels/playlist/item/Metadata.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+  import type { PlaylistItem } from '$lib/schema/playlist';
+
+  interface Props {
+    item: PlaylistItem;
+  }
+
+  let { item }: Props = $props();
+</script>
+
+<div class="metadata">
+  {#if item.tag === 'AirBreak' || item.tag === 'AirBreakWithBackgroundMusic'}
+    <div class="metadata-row" class:air-break-with-bg={item.tag === 'AirBreakWithBackgroundMusic'}>
+      <div>
+        <i class="bi bi-mic"></i>
+        {#if item.tag === 'AirBreakWithBackgroundMusic'}
+          <i>Air break with background music</i>
+        {:else}
+          <i>Air break</i>
+        {/if}
+      </div>
+    </div>
+  {/if}
+  {#if item.tag === 'Song' || item.tag === 'AirBreakWithBackgroundMusic'}
+    <div class="metadata-row">
+      <div>
+        <i class="bi bi-music-note"></i>
+        {#if item.content.title}
+          {item.content.title}
+        {:else}
+          <i>No title</i>
+        {/if}
+      </div>
+
+      <div>
+        <i class="bi bi-person"></i>
+        {#if item.content.artist}
+          {item.content.artist}
+        {:else}
+          <i>No artist</i>
+        {/if}
+      </div>
+    </div>
+
+    <div class="metadata-row bottom">
+      <div>
+        <i class="bi bi-vinyl"></i>
+        {#if item.content.album}
+          {item.content.album}
+        {:else}
+          <i>No album</i>
+        {/if}
+      </div>
+
+      <div>
+        {#if item.internalNotes != '' || item.publicNotes != ''}
+          <i class="bi-sticky"></i>
+        {/if}
+        {#if 'file' in item.content.attributes}
+          <i class="bi-file-earmark"></i>
+        {/if}
+        {#if 'spotify.com' in item.content.attributes}
+          <i class="bi-spotify"></i>
+        {/if}
+        {#if 'music.apple.com' in item.content.attributes}
+          <i class="bi-apple"></i>
+        {/if}
+        {#if 'youtube.com' in item.content.attributes}
+          <i class="bi-youtube"></i>
+        {/if}
+      </div>
+    </div>
+  {/if}
+</div>
+
+<style>
+  .metadata {
+    display: flex;
+    flex-direction: column;
+    gap: 6px;
+  }
+
+  .metadata-row {
+    display: flex;
+    flex-wrap: wrap;
+
+    column-gap: 1em;
+    row-gap: 4px;
+  }
+
+  .metadata-row.air-break-with-bg {
+    padding-bottom: 4px;
+  }
+
+  .metadata-row.bottom {
+    font-size: 0.85em;
+  }
+</style>
